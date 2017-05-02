3b494a3d01f064cb42a5d68b4d4dbc1fd7e7bc6e
Rafael <rafamsilva@live.com>
2017-05-02 10:58:50 -0300

    Componente PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa história</h2>
+        <p>Mussum ipsum</p>
+        <h2>Missão e Visão</h2>
+        <p>Mussum ipsum</p>
+        <h2>Imprensa</h2>
+        <p>Mussum ipsum</p>
     </div>
 )
