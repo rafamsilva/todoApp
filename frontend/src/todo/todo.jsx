ed5f152d6999b5ff41a34ee8d9f04a9c7eaa8124
Rafael <rafamsilva@live.com>
2017-05-02 16:22:41 -0300

    marcar como concluido e pendente

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,6 +14,8 @@ export default class Todo extends Component {
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.refresh()
     }
 
@@ -37,13 +39,29 @@ export default class Todo extends Component {
             .then(resp => this.refresh())
     }
 
+    handleMarkAsDone(todo){
+        axios.put(`${URL}/${todo._id}`, {...todo, done: true})
+            .then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo){
+        axios.put(`${URL}/${todo._id}`, {...todo, done: false})
+            .then(resp => this.refresh())
+    }
+
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm description={this.state.description} handleAdd={this.handleAdd} 
+                <TodoForm 
+                    description={this.state.description} 
+                    handleAdd={this.handleAdd} 
                     handleChange={this.handleChange}/>
-                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
+                <TodoList 
+                    list={this.state.list} 
+                    handleRemove={this.handleRemove}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                    handleMarkAsDone={this.handleMarkAsDone}/>
             </div>
         )
     }
